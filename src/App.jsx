5950badc4a37d50c90cdfb317c68cd8856f72c42
GlickYael <glick.yael3@gmail.com>
2024-09-16 17:55:38 +0300

    Styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,90 @@
 /* eslint-disable no-unused-vars */
 import { useState, useEffect } from "react";
 import "./App.css";
-import WeatherDiv from "./components/WeatherDiv";
 const apiKey = import.meta.env.VITE_API_KEY;
+import WeatherDiv from "./components/WeatherDiv";
+import AppBar from '@mui/material/AppBar';
+import Typography from '@mui/material/Typography';
+import Toolbar from '@mui/material/Toolbar';
+
+
+
 
 function App() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(false);
   const [icons, setIcons] = useState([]);
   const [destinations, setDestinations] = useState([
     "Eilat",
     "London",
     "New York",
-    "Alaska"
+    "Alaska",
+  ]);
+  const [desWeather, setDesWeather] = useState([]);
+  useEffect(() => {
+    initializeDestinations();
+  }, []);
+
+
+  async function initializeDestinations() {
+    setIsLoading(true);
+    try {
+      const weatherDataArray = await Promise.all(
+        destinations.map(async (des) => {
+          const response = await fetch(
+            `https://api.openweathermap.org/data/2.5/weather?q=${des}&appid=${apiKey}&lang=he&units=metric`
+          );
+          const weatherData = await response.json();
+          return weatherData;
+        })
+      );
+      setDesWeather(weatherDataArray);
+    } catch (err) {
+      setError(true);
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
+  return (
+    <>
+      <AppBar position="static" sx={{width:"100%"}}>
+      <Typography variant="h1" color="inherit" component="div">
+      תחזיות מזג האוויר
+    </Typography>
+        </AppBar>
+      <h2>{desWeather.name}</h2>
+      {isLoading && !error && <div>טוען...</div>}
+      {!isLoading && error && (
+        <div>התרחשה שגיאה, לא ניתן לטעון את הטמפרטורות </div>
+      )}
+      <div className="destinations_container">
+        {desWeather.map((desWeather) => {
+          console.log(desWeather);
+          return <WeatherDiv key={desWeather.name} desWeather={desWeather} />;
+        })}
+      </div>
+    </>
+  );
+}
+
+export default App;
+/*
+
+import { useState, useEffect } from "react";
+import "./App.css";
+import WeatherDiv from "./components/WeatherDiv";
+const apiKey = import.meta.env.VITE_API_KEY;
+
+function App() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(false);
+  const [icons, setIcons] = useState([]);
+  const [destinations, setDestinations] = useState([
+    "Eilat",
+    "London",
+    "New York",
+    "Alaska",
   ]);
   const [desWeather, setDesWeather] = useState([]);
   useEffect(() => {
@@ -18,17 +92,23 @@ function App() {
   }, []);
 
   async function initializeDestinations() {
-    const weatherDataArray = await Promise.all(
-      destinations.map(async (des) => {
-        const response = await fetch(
-          `https://api.openweathermap.org/data/2.5/weather?q=${des}&appid=${apiKey}&lang=he&units=metric`
-        );
-        const weatherData = await response.json();
-        return weatherData;
-      })
-    );
-
-    setDesWeather(weatherDataArray);
+    setIsLoading(true);
+    try {
+      const weatherDataArray = await Promise.all(
+        destinations.map(async (des) => {
+          const response = await fetch(
+            `https://api.openweathermap.org/data/2.5/weather?q=${des}&appid=${apiKey}&lang=he&units=metric`
+          );
+          const weatherData = await response.json();
+          return weatherData;
+        })
+      );
+      setDesWeather(weatherDataArray);
+    } catch (err) {
+      setError(true);
+    } finally {
+      setIsLoading(false);
+    }
   }
 
   return (
@@ -36,12 +116,22 @@ function App() {
       <nav>
         <h1>תחזיות מזג האוויר</h1>
       </nav>
-      {desWeather.map((desWeather) => {
-        console.log(desWeather);
-        return <WeatherDiv key={desWeather.name} desWeather={desWeather} />;
-      })}
+      <h2>{desWeather.name}</h2>
+      {isLoading && !error && <div>טוען...</div>}
+      {!isLoading && error && (
+        <div>התרחשה שגיאה, לא ניתן לטעון את הטמפרטורות </div>
+      )}
+      {!destinations.name
+        ? null
+        : 
+      desWeather.map((desWeather) => {
+            console.log(desWeather);
+            return <WeatherDiv key={desWeather.name} desWeather={desWeather} />;
+          })}
     </>
   );
 }
 
 export default App;
+
+*/
--- src/components/WeatherDiv.jsx
+++ src/components/WeatherDiv.jsx
@@ -1,17 +1,37 @@
+/* eslint-disable no-unused-vars */
 /* eslint-disable react/prop-types */
-function WeatherDiv({desWeather}) {
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import Typography from "@mui/material/Typography";
+import WbSunnyIcon from '@mui/icons-material/WbSunny';
+import CloudIcon from '@mui/icons-material/Cloud';
+import FlashOnIcon from '@mui/icons-material/FlashOn';
+import Grid from '@mui/material/Grid2';
 
-  return (
-    <div>
-        <h2>{desWeather.name}</h2>
-        <ul>
-            <li>{desWeather.weather[0].description}</li>
-            <li>הטמפרטורה {desWeather.main.temp} C</li>
-            <li>מרגיש כמו: {desWeather.main.feels_like}</li>
-            <li>אחוזי הלחות{desWeather.main.humidity}%</li>
-        </ul>
-    </div>
-  )
+import {useState} from "react"
+
+function WeatherDiv({ desWeather }) {
+  const [icon,setIcon]= useState((desWeather.main.temp<=20)?(<FlashOnIcon sx={{color:"#3f51b5"}}/>):((desWeather.main.temp<30)?<CloudIcon sx={{color:"#42a5f5"}}/>:<WbSunnyIcon sx={{color:"#f57c00"}}/>))
+  return ( 
+    <Card sx={{ minWidth: 275, backgroundColor: "#e3f2fd", margin: 5 }}>
+      <CardContent>
+        <Grid container justifyContent="space-between" spacing={2} sx={{textAlign:"right"}}>
+          <Grid>
+            <Grid>
+              <Typography variant="h3" component="p" sx={{ fontWeight: 900, color: "text.secondary" }}>{desWeather.name}</Typography>
+              <Typography component="p" sx={{ color: "text.secondary" }} >{desWeather.weather[0].description}</Typography>
+            </Grid>
+            <Grid >
+              <Grid><p>טמפרטורה נמדדת: {desWeather.main.temp}°C</p></Grid>
+              <Grid><p>טמפרטורה מורגשת: {desWeather.main.feels_like}°C</p></Grid>
+              <Grid><p>אחוזי הלחות: {desWeather.main.humidity}%</p></Grid>
+            </Grid>
+          </Grid>
+          <Grid><p>{icon}</p></Grid>
+        </Grid> 
+      </CardContent>
+    </Card>
+  );
 }
 
 export default WeatherDiv;

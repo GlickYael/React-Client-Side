649af2c8f01b716c37891b094657a8c7b4fcc553
GlickYael <glick.yael3@gmail.com>
2024-09-16 15:16:45 +0300

    Base structure with the real API

--- src/App.jsx
+++ src/App.jsx
@@ -1,52 +1,44 @@
 /* eslint-disable no-unused-vars */
 import { useState, useEffect } from "react";
 import "./App.css";
-import env from "react-dotenv";
 import WeatherDiv from "./components/WeatherDiv";
+const apiKey = import.meta.env.VITE_API_KEY;
+
 function App() {
   const [icons, setIcons] = useState([]);
-  const [data, setData]= useState([]);
   const [destinations, setDestinations] = useState([
-    { keys: "Eilat", name: "אילת" },
-    { keys: "London", name: "לונדון" },
-    { keys: "New York", name: "ניו יורק" },
-    { keys: "Alaska,", name: "אלסקה" },
+    "Eilat",
+    "London",
+    "New York",
+    "Alaska"
   ]);
-  /*
-    { keys: "Eilat,il", name: "אילת" },
-    { keys: "London,uk", name: "לונדון" },
-    { keys: "New York,us", name: "ניו יורק" },
-    { keys: "Alaska,us", name: "אלסקה" }, */
+  const [desWeather, setDesWeather] = useState([]);
   useEffect(() => {
     initializeDestinations();
   }, []);
-  //option 1.
-  async function initializeDestinations() {
-    destinations.forEach(async (des) => {
-      const response = await fetch("https://jsonplaceholder.typicode.com/users/1", {
-        method: "GET",
-        headers: {
-          "Content-Type": "application/json",
-        },
-      });
-      const weatherData = await response.json();
-      const updatedDestination = { ...des, weatherData };
 
-      // Update the state for each destination individually
-      setDestinations((prevDestinations) =>
-        prevDestinations.map((d) =>
-          d.keys === des.keys ? updatedDestination : d
-        )
-      );
-    });
+  async function initializeDestinations() {
+    const weatherDataArray = await Promise.all(
+      destinations.map(async (des) => {
+        const response = await fetch(
+          `https://api.openweathermap.org/data/2.5/weather?q=${des}&appid=${apiKey}&lang=he&units=metric`
+        );
+        const weatherData = await response.json();
+        return weatherData;
+      })
+    );
+
+    setDesWeather(weatherDataArray);
   }
-  
+
   return (
     <>
-      <h1>hhh</h1>
-      {destinations.map((des) => {
-        console.log(des);
-        return <WeatherDiv key={des.keys} destination={des} />;
+      <nav>
+        <h1>תחזיות מזג האוויר</h1>
+      </nav>
+      {desWeather.map((desWeather) => {
+        console.log(desWeather);
+        return <WeatherDiv key={desWeather.name} desWeather={desWeather} />;
       })}
     </>
   );
--- src/components/WeatherDiv.jsx
+++ src/components/WeatherDiv.jsx
@@ -1,14 +1,14 @@
 /* eslint-disable react/prop-types */
-function WeatherDiv({destination}) {
-    
+function WeatherDiv({desWeather}) {
+
   return (
     <div>
-        <h2>שם: {destination.name}</h2>
+        <h2>{desWeather.name}</h2>
         <ul>
-            <li>{destination.description}</li>
-            <li>הטמפרטורה {destination.temp}</li>
-            <li>{destination.feels_like}</li>
-            <li>{destination.humidity}</li>
+            <li>{desWeather.weather[0].description}</li>
+            <li>הטמפרטורה {desWeather.main.temp} C</li>
+            <li>מרגיש כמו: {desWeather.main.feels_like}</li>
+            <li>אחוזי הלחות{desWeather.main.humidity}%</li>
         </ul>
     </div>
   )
